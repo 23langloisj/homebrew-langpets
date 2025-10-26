7685b8aa49f17789db1187c2af02884c99df4b8e
Jake Langlois <langlois.j@northeastern.edu>
2025-10-26 16:38:13 -0400

    new sha

--- Formula/langpets.rb
+++ Formula/langpets.rb
@@ -2,7 +2,7 @@ class Langpets < Formula
   desc "A friendly little terminal pet that reacts to how you treat it 🐢"
   homepage "https://github.com/23langloisj/langpets"
   url "https://github.com/23langloisj/langpets/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3dae30ea9be8926c6b05b4fbf529018cacabc9b541840738eafbd35d9dff23c7"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "python@3.11"
