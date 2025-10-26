47411031df6ae514d81977d95f916a24916a4922
Jake Langlois <langlois.j@northeastern.edu>
2025-10-26 16:10:52 -0400

    updating skeleton

--- Formula/langpets.rb
+++ Formula/langpets.rb
@@ -1,18 +1,17 @@
 class Langpets < Formula
-    desc "A friendly little terminal pet that reacts to how you treat it 🐢"
-    homepage "https://github.com/<yourusername>/langpets"
-    url "https://github.com/<yourusername>/langpets/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "<insert-sha256-checksum>"
-    license "MIT"
-  
-    depends_on "python@3.11"
-  
-    def install
-      bin.install "langpets"
-    end
-  
-    test do
-      system "#{bin}/langpets", "--help"
-    end
+  desc "A friendly little terminal pet that reacts to how you treat it 🐢"
+  homepage "https://github.com/23langloisj/langpets"
+  url "https://github.com/23langloisj/langpets/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "<REPLACE_WITH_REAL_SHA256>"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    bin.install "langpets"
   end
-  
+
+  test do
+    system "#{bin}/langpets", "--help"
+  end
+end
