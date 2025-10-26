78a7a6d7c720cd69bce36acc5e5dede634a36081
Jake Langlois <langlois.j@northeastern.edu>
2025-10-26 17:01:33 -0400

    updated formula

--- Formula/langpets.rb
+++ Formula/langpets.rb
@@ -1,8 +1,8 @@
 class Langpets < Formula
   desc "A friendly little terminal pet that reacts to how you treat it 🐢"
   homepage "https://github.com/23langloisj/langpets"
-  url "https://github.com/23langloisj/langpets/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3dae30ea9be8926c6b05b4fbf529018cacabc9b541840738eafbd35d9dff23c7"
+  url "https://github.com/23langloisj/langpets/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "ae92e2afcc9aa88031e9df08f312700033d7a01215dfa82c4b291b267d0e2052"
   license "MIT"
 
   depends_on "python@3.11"
